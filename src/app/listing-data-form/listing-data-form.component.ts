2345a721a6a9202b1b4702d917a3f85651302491
703Pvrtcx <kasaokapapkies@gmail.com>
2023-06-05 17:37:14 +0200

    feat: added a new dynamic form for adding and editing

--- src/app/listing-data-form/listing-data-form.component.ts
+++ src/app/listing-data-form/listing-data-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { fakeListings } from '../fake-data';
 import { Listing } from '../types';
@@ -9,16 +9,26 @@ import { Listing } from '../types';
   styleUrls: ['./listing-data-form.component.css']
 })
 export class ListingDataFormComponent implements OnInit {
+  @Input() buttonText:any;
   name: string = '';
   description: string = '';
   price: string ='';
+
+  @Output() onSubmit = new EventEmitter<Listing>();
   constructor(  private router: Router) { }
 
   ngOnInit(): void {
   }
-  onSubmit():void{
-    alert('Creating a new listing...');
-    this.router.navigateByUrl('/my-listings');
+  onButtonClicked():void{
+    this.onSubmit.emit({
+      id: '',
+      name: this.name,
+      description: this.description,
+      price: Number(this.price)
+    })
+
+    // alert('Creating a new listing...');
+    // this.router.navigateByUrl('/my-listings');
   }
 }
 
